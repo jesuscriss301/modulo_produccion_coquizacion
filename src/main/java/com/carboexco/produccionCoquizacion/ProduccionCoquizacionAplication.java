64abbabf54148fc89f040d81af4069c94addc1b9
Carboexco2 <124207086+Carboexco2@users.noreply.github.com>
2023-06-09 10:21:06 -0500

    inicializar de proyectos

---
+++ src/main/java/com/carboexco/produccionCoquizacion/ProduccionCoquizacionAplication.java
@@ -0,0 +1,13 @@
+package com.carboexco.produccionCoquizacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduccionCoquizacionAplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduccionCoquizacionAplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carboexco/produccionCoquizacion/ProduccionCoquizacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carboexco.produccionCoquizacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduccionCoquizacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

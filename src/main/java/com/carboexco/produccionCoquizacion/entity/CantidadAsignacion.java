1365b42a26653945722ceaa97be904046cbdb9e4
Carboexco2 <124207086+Carboexco2@users.noreply.github.com>
2023-06-14 12:02:08 -0500

    cantidadAsignacion

--- src/main/java/com/carboexco/produccionCoquizacion/entity/CantidadAsignacion.java
+++ src/main/java/com/carboexco/produccionCoquizacion/entity/CantidadAsignacion.java
@@ -1,18 +1,42 @@
 package com.carboexco.produccionCoquizacion.entity;
 
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.Hibernate;
+
+import java.io.Serializable;
+import java.util.Objects;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "cantidad_asignacion")
-public class CantidadAsignacion {
-    @EmbeddedId
-    private CantidadAsignacionId id;
+public class CantidadAsignacion implements Serializable {
+    private static final long serialVersionUID = 2938135034729026791L;
 
-    //TODO [JPA Buddy] generate columns from DB
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "id_asignacion", nullable = false)
+    private Integer idAsignacion;
+
+    @Column(name = "id_tipo_asignacion", nullable = false)
+    private Integer idTipoAsignacion;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        CantidadAsignacion entity = (CantidadAsignacion) o;
+        return Objects.equals(this.idAsignacion, entity.idAsignacion) &&
+                Objects.equals(this.idTipoAsignacion, entity.idTipoAsignacion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idAsignacion, idTipoAsignacion);
+    }
+
 }
--- src/main/java/com/carboexco/produccionCoquizacion/repository/CantidadAsignacionRepository.java
+++ src/main/java/com/carboexco/produccionCoquizacion/repository/CantidadAsignacionRepository.java
@@ -1,7 +1,6 @@
 package com.carboexco.produccionCoquizacion.repository;
 
 import com.carboexco.produccionCoquizacion.entity.CantidadAsignacion;
-import com.carboexco.produccionCoquizacion.entity.CantidadAsignacionId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CantidadAsignacionRepository extends JpaRepository<CantidadAsignacion, CantidadAsignacionId> {

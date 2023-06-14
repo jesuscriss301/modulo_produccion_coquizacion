3f05fa1f6d4492b134a0d6bd35709f5c20b55114
Carboexco2 <124207086+Carboexco2@users.noreply.github.com>
2023-06-14 11:46:18 -0500

    controlador de asignacion

---
+++ src/main/java/com/carboexco/produccionCoquizacion/controller/AsignacionController.java
@@ -0,0 +1,14 @@
+package com.carboexco.produccionCoquizacion.controller;
+
+
+import com.carboexco.produccionCoquizacion.entity.Apagado;
+import com.carboexco.produccionCoquizacion.repository.ApagadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+public class AsignacionController {
+}

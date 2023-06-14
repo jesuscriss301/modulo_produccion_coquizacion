484c676e81962ba0f47212855967f2dd10cc132a
Carboexco2 <124207086+Carboexco2@users.noreply.github.com>
2023-06-14 14:31:36 -0500

    controlador de asignacion

--- src/main/java/com/carboexco/produccionCoquizacion/controller/AsignacionController.java
+++ src/main/java/com/carboexco/produccionCoquizacion/controller/AsignacionController.java
@@ -1,8 +1,7 @@
 package com.carboexco.produccionCoquizacion.controller;
 
-
-import com.carboexco.produccionCoquizacion.entity.Apagado;
-import com.carboexco.produccionCoquizacion.repository.ApagadoRepository;
+import com.carboexco.produccionCoquizacion.entity.Asignacion;
+import com.carboexco.produccionCoquizacion.repository.AsignacionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,5 +9,61 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+@RestController
+@RequestMapping("/asignaciones")
 public class AsignacionController {
+
+    @Autowired
+    private AsignacionRepository asignacionRepository;
+
+    @GetMapping
+    public List<Asignacion> getAllAsignaciones() {
+        return asignacionRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Asignacion> getAsignacionById(@PathVariable Integer id) {
+        Optional<Asignacion> asignacionOptional = asignacionRepository.findById(id);
+
+        if (asignacionOptional.isPresent()) {
+            Asignacion asignacion = asignacionOptional.get();
+            return ResponseEntity.ok(asignacion);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Asignacion> createAsignacion(@RequestBody Asignacion asignacion) {
+        Asignacion createdAsignacion = asignacionRepository.save(asignacion);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdAsignacion);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Asignacion> updateAsignacion(@PathVariable Integer id, @RequestBody Asignacion asignacion) {
+        Optional<Asignacion> asignacionOptional = asignacionRepository.findById(id);
+
+        if (asignacionOptional.isPresent()) {
+            Asignacion existingAsignacion = asignacionOptional.get();
+            // Actualizar los campos necesarios de existingAsignacion con los valores de asignacion recibidos en el body
+
+            Asignacion updatedAsignacion = asignacionRepository.save(existingAsignacion);
+            return ResponseEntity.ok(updatedAsignacion);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAsignacion(@PathVariable Integer id) {
+        Optional<Asignacion> asignacionOptional = asignacionRepository.findById(id);
+
+        if (asignacionOptional.isPresent()) {
+            asignacionRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
+
